f7b26af2d3a523631f74c1a5368dd0c78b8566f1
89942589@qq.com <3.14159265358979>
2018-05-29 23:08:58 +0800

    更新

--- lxb_blog/views.py
+++ lxb_blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from lxb_blog.models import Article
 
 # Create your views here.
+
+def home(request):
+    post_list = Article.objects.all()  #获取全部的Article对象
+    return render(request, 'home.html', {'post_list' : post_list})
