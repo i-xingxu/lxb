26830855ac1e563537e83ab48cc6b71e15b591f2
89942589@qq.com <3.14159265358979>
2018-05-30 22:41:38 +0800

    添加模板网页

--- lxb_blog/views.py
+++ lxb_blog/views.py
@@ -5,4 +5,4 @@ from lxb_blog.models import Article
 
 def home(request):
     post_list = Article.objects.all()  #获取全部的Article对象
-    return render(request, 'home.html', {'post_list' : post_list})
+    return render(request, 'index.html')
