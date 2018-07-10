cd2a7ce517290c2f8ea4b5c4337a41df0e0032f8
xingxu <89942589@qq.com>
2018-07-10 23:35:59 +0800

    后台传值到前台

--- lxb_blog/views.py
+++ lxb_blog/views.py
@@ -5,4 +5,6 @@ from lxb_blog.models import Article
 
 def home(request):
     post_list = Article.objects.all()  #获取全部的Article对象
-    return render(request, 'index.html')
+    context={}
+    context["imagePath"]="../static/images/1.jpg"
+    return render(request, 'index.html',context)
